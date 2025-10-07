9a19d1793cb07ec9f096bbf96f0c5378c38ab8b0
patastats10 <pata.stats10@gmail.com>
2025-10-07 22:04:02 +0300

    Update csv_download.py

--- csv_download.py
+++ csv_download.py
@@ -1454,7 +1454,7 @@ if uploaded_file:
         if outcome == "Goal":
             linestyle_ = linestyles.get(outcome, '-')
             marker_handles.append(
-                pitch.scatter([], [], color='w', linestyle=linestyle_, marker=marker, s=300, edgecolor='k', ax=axs_Shots)
+                pitch.scatter([], [], hexcolor='w', pentcolor='k', linestyle=linestyle_, marker=marker, s=300,  ax=axs_Shots)
             )
         else:
             linestyle_ = linestyles.get(outcome, '-')
