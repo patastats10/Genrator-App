6e22d89d459c06c8097c8b34f81d92f9277f0b0d
patastats10 <pata.stats10@gmail.com>
2025-11-05 20:36:04 +0200

    Update csv_download.py

--- csv_download.py
+++ csv_download.py
@@ -1631,7 +1631,7 @@ if uploaded_file:
     goal_height = 2.67
     depth = 0.6
     
-    fig_Shots_on_frame, ax_Shots_on_frame = plt.subplots(figsize=(25,  14.25))
+    fig_Shots_on_frame, ax_Shots_on_frame = plt.subplots(figsize=(30,  16))
     
     # إحداثيات الشبكة الخلفية
     back_left = (depth, 0.25)
